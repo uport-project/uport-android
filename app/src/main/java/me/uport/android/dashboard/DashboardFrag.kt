b3f70e6b6bfbaa2dd9659b8f903b8bf01fa04ec5
Mircea Nistor <mirceanis@gmail.com>
2018-06-11 13:03:51 +0300

    add navigation to [unimplemented] profile editing screen

--- app/src/main/java/me/uport/android/dashboard/DashboardFrag.kt
+++ app/src/main/java/me/uport/android/dashboard/DashboardFrag.kt
@@ -21,6 +21,11 @@ class DashboardFrag : Fragment() {
     private val navController by lazy { NavHostFragment.findNavController(this) }
     private var tabPagerAdapter: TabPagerAdapter? = null
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setHasOptionsMenu(true)
+    }
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_dashboard, container, false)
--- app/src/main/java/me/uport/android/profile/UserProfileFrag.kt
+++ app/src/main/java/me/uport/android/profile/UserProfileFrag.kt
@@ -3,25 +3,42 @@ package me.uport.android.profile
 import android.arch.lifecycle.ViewModelProviders
 import android.os.Bundle
 import android.support.v4.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
+import androidx.navigation.fragment.NavHostFragment
+import kotlinx.android.synthetic.main.fragment_dashboard.*
 
 import me.uport.android.R
 
 class UserProfileFrag : Fragment() {
 
     private lateinit var viewModel: UserProfileViewModel
+    private val navController by lazy { NavHostFragment.findNavController(this) }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setHasOptionsMenu(true)
+    }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_user_profile, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        profile_container.setOnClickListener({ v -> navController.navigate(R.id.action_profile_to_edit_profile) })
+    }
+
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProviders.of(this).get(UserProfileViewModel::class.java)
         // TODO: Use the ViewModel
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.menu_profile, menu)
     }
 
 }
