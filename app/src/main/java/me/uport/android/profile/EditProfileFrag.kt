6c625ceccd598b877c76a6bfbac057b63c3c4515
Mircea Nistor <mirceanis@gmail.com>
2018-06-11 13:19:13 +0300

    add stub profile editing screen

---
+++ app/src/main/java/me/uport/android/profile/EditProfileFrag.kt
@@ -0,0 +1,27 @@
+package me.uport.android.profile
+
+import android.arch.lifecycle.ViewModelProviders
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+import me.uport.android.R
+
+class EditProfileFrag : Fragment() {
+
+    private lateinit var viewModel: UserProfileViewModel
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_edit_profile, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProviders.of(this).get(UserProfileViewModel::class.java)
+        // TODO: Use the ViewModel
+    }
+
+}
